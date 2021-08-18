2d92a876b40e74af3d6d521fac4ecdf3f42c8c3d
champion <243342181@qq.com>
2021-08-18 23:16:58 +0800

    创建文件服务器

--- build_tools/生成filelist-Android.bat
+++ build_tools/生成filelist-Android.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 set bak_path=%~dp0
 set dst_path=%~dp0..\KSFramework\Product\Bundles\Android\
-py gen_filelist.py %dst_path% %bak_path% "Android"
+python gen_filelist.py %dst_path% %bak_path% "Android"
--- build_tools/生成更新资源包-Windows.bat
+++ build_tools/生成更新资源包-Windows.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 set start_path=%~dp0
-py gen_hotfix_res.py %start_path% "Windows"
+python gen_hotfix_res.py %start_path% "Windows"
